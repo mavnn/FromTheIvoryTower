0d72971dc4a49a83e20ff22b8ce1652b1c411de8
mavnn <michael@mavnn.co.uk>
2016-04-15 07:33:27 +0100

    And again...

--- slides/index.fsx
+++ slides/index.fsx
@@ -461,10 +461,8 @@ Training and consultancy through http://mavnn.co.uk
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
   ga('create', 'UA-37561687-1', 'auto');
   ga('send', 'pageview');
-
 </script>
 
 *)
